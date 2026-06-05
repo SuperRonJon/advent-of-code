a492baeac912ac0e23e4697dfb919280f0591720
Patrick Seute <patrick.seute@cox.net>
2026-06-04 22:34:26 -0400

    add 2024 day 1 part 1

---
+++ 2024/01/part1.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINESIZE 128
+
+size_t count_lines(FILE *f)
+{
+	char linebuff[LINESIZE];
+	size_t count = 0;
+	while (fgets(linebuff, LINESIZE, f) != NULL)
+	{
+		count++;
+	}
+	clearerr(f);
+	rewind(f);
+	return count;
+}
+
+struct tupl
+{
+	int first;
+	int second;
+};
+
+struct tupl split_nums(char *s)
+{
+	int found_first = 0;
+	size_t slen = strlen(s);
+	char *curr = malloc(slen);
+	size_t curri = 0;
+	int first = 0;
+	int second = 0;
+	for (size_t i = 0; i < slen; i++)
+	{
+		if (s[i] != ' ' && s[i] != '\n')
+		{
+			curr[curri++] = s[i];
+		}
+		else if (found_first == 0)
+		{
+			curr[curri] = '\0';
+			first = strtol(curr, NULL, 10);
+			curri = 0;
+			found_first = 1;
+		}
+	}
+	curr[curri] = '\0';
+	second = strtol(curr, NULL, 10);
+	struct tupl t = {first, second};
+	free(curr);
+	return t;
+}
+
+int cmp_ints(const void *left, const void *right)
+{
+	const int first = *(int *)left;
+	const int second = *(int *)right;
+	if (first == second)
+		return 0;
+	if (first < second)
+		return -1;
+	return 1;
+}
+
+int get_total_distance(const int *l1, const int *l2, const size_t len)
+{
+	int total = 0;
+	for (size_t i = 0; i < len; i++)
+	{
+		int dist = l1[i] - l2[i];
+		if (dist < 0)
+			dist *= -1;
+		total += dist;
+	}
+	return total;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fputs("No input file given\n", stderr);
+		return 1;
+	}
+
+	FILE *infile = fopen(argv[1], "r");
+	size_t line_count = count_lines(infile);
+	int *list1 = malloc(line_count * sizeof(int));
+	int *list2 = malloc(line_count * sizeof(int));
+	size_t i = 0;
+	char linebuff[LINESIZE];
+	while (fgets(linebuff, LINESIZE, infile) != NULL)
+	{
+		struct tupl t = split_nums(linebuff);
+		list1[i] = t.first;
+		list2[i] = t.second;
+		i++;
+	}
+	qsort(list1, line_count, sizeof(int), cmp_ints);
+	qsort(list2, line_count, sizeof(int), cmp_ints);
+	int dist = get_total_distance(list1, list2, line_count);
+	printf("Total distance: %d\n", dist);
+	free(list1);
+	free(list2);
+	return 0;
+}
